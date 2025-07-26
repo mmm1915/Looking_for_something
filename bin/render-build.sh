a95dedde707784ac58fbe3ca3e7d605f5e551add
mmm1915 <15mnm51@gmail.com>
2025-07-26 23:00:16 +0900

    Add render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,7 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
+
+RAILS_ENV=production bundle exec rails assets:precompile
+RAILS_ENV=production bundle exec rails assets:clean
+RAILS_ENV=production bundle exec rails db:migrate
