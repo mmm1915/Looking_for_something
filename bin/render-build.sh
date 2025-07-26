ce1d40f74f32b77185ceee9e6657b37776fd4f18
mmm1915 <15mnm51@gmail.com>
2025-07-27 00:27:31 +0900

    マイグレーションを直接buildCommandで実行するよう変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,7 @@
 set -o errexit
 
 bundle install
-
-RAILS_ENV=production bundle exec rails assets:precompile
-RAILS_ENV=production bundle exec rails assets:clean
-RAILS_ENV=production bundle exec rails db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+# bundle exec rake db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
