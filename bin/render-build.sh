ef2bba170bdb519fd81703f764d9327b19936d75
mmm1915 <15mnm51@gmail.com>
2025-07-27 23:15:59 +0900

    render-build.shを修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,5 @@ bundle install
 
 RAILS_ENV=production bundle exec rails assets:precompile
 RAILS_ENV=production bundle exec rails assets:clean
+RAILS_ENV=production bundle exec rails db:create 
 RAILS_ENV=production bundle exec rails db:migrate
