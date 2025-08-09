6d0b83be8a1dbf3271aafbf7354479675c265ba5
mmm1915 <15mnm51@gmail.com>
2025-08-09 16:45:37 +0900

    トップページの修正

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,8 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  def new
+    super do |resource|
+      resource.nickname = ::Faker::JapaneseMedia::StudioGhibli.unique.character
+    end
+  end
+end
