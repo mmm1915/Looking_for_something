86733afefe2a31119fa9d23cf75eb72c8542d856
mmm1915 <15mnm51@gmail.com>
2025-08-15 23:38:43 +0900

    戻るボタン追加

--- app/models/search_post.rb
+++ app/models/search_post.rb
@@ -4,7 +4,7 @@ class SearchPost < ApplicationRecord
   enum status: { searching: 0, found: 1 }
 
   validates :item_name, presence: true
-  validates :found_place, presence: true, if: :found?
+  validate :found_place_presence_if_found
 
   def self.ransackable_attributes(auth_object = nil)
     %w[created_at found_place item_name search_place status user_id]
@@ -12,5 +12,11 @@ class SearchPost < ApplicationRecord
 
   def self.ransackable_associations(auth_object = nil)
     %w[user]
+  end
+
+  def found_place_presence_if_found
+    if status == "found" && found_place.blank?
+      errors.add(:found_place, "を入力してください")
+    end
   end
 end
